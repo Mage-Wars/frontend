2d324c7e426da15d14161fe800697d358600ecc7
Oleg Papulov <raffoq8@gmail.com>
2020-12-20 22:45:51 +0300

    setup webpack setup typescript setup env

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { App } from './App';
+import { App } from '@/App';
 
-const ROOT = document.getElementById('#root');
+const ROOT = document.getElementById('root');
 
 ReactDOM.render(<App />, ROOT);
